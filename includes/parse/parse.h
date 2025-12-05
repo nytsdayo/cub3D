f7772821f6f312f86bcd217a6a3eee857c55db51
nytsdayo <150505811+nytsdayo@users.noreply.github.com>
2025-12-05 19:15:28 +0000

    style: auto-fix formatting for Norminette compliance

--- includes/parse/parse.h
+++ includes/parse/parse.h
@@ -11,45 +11,40 @@
 /* ************************************************************************** */
 
 #ifndef PARSE_H
-# define PARSE_H
+#define PARSE_H
 
-# include <stddef.h>
+#include <stddef.h>
 
 /* データ構造 */
 
-typedef struct s_color
-{
-	int	r;
-	int	g;
-	int	b;
-}	t_color;
+typedef struct s_color {
+  int r;
+  int g;
+  int b;
+} t_color;
 
-typedef struct s_config_data
-{
-	char	*north_texture_path;
-	char	*south_texture_path;
-	char	*west_texture_path;
-	char	*east_texture_path;
-	t_color	floor_color;
-	t_color	ceiling_color;
-}	t_config_data;
+typedef struct s_config_data {
+  char *north_texture_path;
+  char *south_texture_path;
+  char *west_texture_path;
+  char *east_texture_path;
+  t_color floor_color;
+  t_color ceiling_color;
+} t_config_data;
 
-typedef struct s_map_data
-{
-	char	**map;
-}	t_map_data;
+typedef struct s_map_data {
+  char **map;
+} t_map_data;
 
-typedef struct s_game_data
-{
-	t_config_data	config;
-	t_map_data		map;
-}	t_game_data;
+typedef struct s_game_data {
+  t_config_data config;
+  t_map_data map;
+} t_game_data;
 
 /* 公開関数 */
 
-int	parse(const char **map);
-int	parse_config(char **input_data, size_t *line_index,
-		t_config_data *config);
-int	parse_map(char **input_data, size_t line_index, t_map_data *map_data);
+int parse(const char **map);
+int parse_config(char **input_data, size_t *line_index, t_config_data *config);
+int parse_map(char **input_data, size_t line_index, t_map_data *map_data);
 
 #endif
--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -11,27 +11,26 @@
 /* ************************************************************************** */
 
 #include "parse.h"
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 
 /**
  * @brief メインのパース関数
  * @param map 入力データ
  * @return 成功: 0 / 失敗: エラーコード
  */
-int	parse(const char **map)
-{
-	t_game_data	game_data;
-	size_t		line_index;
-	int			result;
+int parse(const char **map) {
+  t_game_data game_data;
+  size_t line_index;
+  int result;
 
-	line_index = 0;
-	result = parse_config((char **)map, &line_index, &game_data.config);
-	if (result != 0)
-		return (result);
-	result = parse_map((char **)map, line_index, &game_data.map);
-	if (result != 0)
-		return (result);
-	return (0);
+  line_index = 0;
+  result = parse_config((char **)map, &line_index, &game_data.config);
+  if (result != 0)
+    return (result);
+  result = parse_map((char **)map, line_index, &game_data.map);
+  if (result != 0)
+    return (result);
+  return (0);
 }
