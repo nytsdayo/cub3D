6b8cdfbd01d0ceab70a9cb9070c89265c18c85b8
misato <kawano.misato@gmail.com>
2025-12-08 00:34:12 +0900

    feat: implement player movement and rotation
      - Add WASD movement (forward, backward, left, right)
      - Add arrow key rotation (left, right)
      - Implement collision detection (wall checking)
      - Create player module with movement and rotation functions
      - Update key handler to support player controls
    
      Files added:
      - srcs/engine/player/player_movement.c
      - srcs/engine/player/player_rotation.c
      - includes/engine/player.h
    
      Files modified:
      - srcs/utils/cleanup.c (extended handle_keypress)
      - Makefile (added player source files)
    
      Norminette: All new files pass

---
+++ includes/engine/player.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkawano <mkawano@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 23:26:58 by mkawano           #+#    #+#             */
+/*   Updated: 2025/12/07 23:27:01 by mkawano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PLAYER_H
+# define PLAYER_H
+
+# include "cub3d.h"
+
+/* Movement */
+void	move_forward(t_game *game);
+void	move_backward(t_game *game);
+void	move_left(t_game *game);
+void	move_right(t_game *game);
+
+/* Rotation */
+void	rotate_left(t_game *game);
+void	rotate_right(t_game *game);
+
+#endif
