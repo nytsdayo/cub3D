b5e60c726e143d0fb84a833df83b5a74ce79cfc3
nytsdayo <nyantsutsu@gmail.com>
2025-12-20 00:55:06 +0900

    apply mapfile_path_change

--- tests/parse/map/test_parse_map.cpp
+++ tests/parse/map/test_parse_map.cpp
@@ -6,7 +6,7 @@
 /*   By: rnakatan <rnakatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 09:00:00 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/12/19 19:15:39 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/12/20 00:46:24 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int	main(void)
 	std::cout << "===== Parse Map Module Tests =====" << std::endl;
 	
 	// Valid maps
-	test_map_file("tests/parse/map/valid_simple.cub", 0);
-	test_map_file("tests/parse/map/valid_medium.cub", 0);
-	test_map_file("tests/parse/map/valid_with_spaces.cub", 0);
+	test_map_file("test_data/map/valid_simple.cub", 0);
+	test_map_file("test_data/map/valid_medium.cub", 0);
+	test_map_file("test_data/map/valid_with_spaces.cub", 0);
 	
 	// Invalid maps
-	test_map_file("tests/parse/map/invalid_no_player.cub", -1);
-	test_map_file("tests/parse/map/invalid_two_players.cub", -1);
-	test_map_file("tests/parse/map/invalid_no_wall.cub", -1);
-	test_map_file("tests/parse/map/invalid_too_small.cub", -1);
-	test_map_file("tests/parse/map/invalid_bad_char.cub", -1);
+	test_map_file("test_data/map/invalid_no_player.cub", -1);
+	test_map_file("test_data/map/invalid_two_players.cub", -1);
+	test_map_file("test_data/map/invalid_no_wall.cub", -1);
+	test_map_file("test_data/map/invalid_too_small.cub", -1);
+	test_map_file("test_data/map/invalid_bad_char.cub", -1);
 	
 	std::cout << "\n===== All Tests Completed =====" << std::endl;
 	return 0;
