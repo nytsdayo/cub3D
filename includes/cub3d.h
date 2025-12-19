6a3fbe6edbfb4097540c5fb89fc7b498d4d38d3f
nytsdayo <nyantsutsu@gmail.com>
2025-12-20 00:55:56 +0900

    apply norminette

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: rnakatan <rnakatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:05:14 by mkawano           #+#    #+#             */
-/*   Updated: 2025/12/13 21:23:53 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/12/20 00:42:57 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,43 +20,43 @@
 # include <unistd.h>
 
 # ifdef __linux__
-# include <X11/X.h>
-# include <X11/keysym.h>
-#endif
+#  include <X11/X.h>
+#  include <X11/keysym.h>
+# endif
 
 # define WINDOW_WIDTH 800
 # define WINDOW_HEIGHT 600
 # define TITLE "cub3D"
 
 /* Raycasting Constants */
-#define MAP_WIDTH 8
-#define MAP_HEIGHT 8
-#define FOV 60.0
-#define MOVE_SPEED 0.1
-#define ROT_SPEED 0.05
-#define COLLISION_MARGIN 0.2
+# define MAP_WIDTH 8
+# define MAP_HEIGHT 8
+# define FOV 60.0
+# define MOVE_SPEED 0.1
+# define ROT_SPEED 0.05
+# define COLLISION_MARGIN 0.2
 
 /* Key State Array Size (large enough for X11 KeySym values) */
-#define KEY_STATE_SIZE 70000
+# define KEY_STATE_SIZE 70000
 
 /* Key Codes (Cross-platform) */
 # ifdef __APPLE__
-# define KEY_ESC 53
-# define KEY_W 13
-# define KEY_A 0
-# define KEY_S 1
-# define KEY_D 2
-# define KEY_LEFT 123
-# define KEY_RIGHT 124
+#  define KEY_ESC 53
+#  define KEY_W 13
+#  define KEY_A 0
+#  define KEY_S 1
+#  define KEY_D 2
+#  define KEY_LEFT 123
+#  define KEY_RIGHT 124
 # else
-# define KEY_ESC XK_Escape
-# define KEY_W XK_w
-# define KEY_A XK_a
-# define KEY_S XK_s
-# define KEY_D XK_d
-# define KEY_LEFT XK_Left
-# define KEY_RIGHT XK_Right
-#endif
+#  define KEY_ESC XK_Escape
+#  define KEY_W XK_w
+#  define KEY_A XK_a
+#  define KEY_S XK_s
+#  define KEY_D XK_d
+#  define KEY_LEFT XK_Left
+#  define KEY_RIGHT XK_Right
+# endif
 
 /* Events */
 # define ON_KEYDOWN 2
@@ -85,14 +85,15 @@ typedef struct s_img
 }			t_img;
 
 /* Game Structure */
-typedef struct s_game {
-  void *mlx;
-  void *win;
-  char **map;
-  int   world_map[MAP_HEIGHT][MAP_WIDTH];
-  t_player player;
-  t_img img;
-  int keys[KEY_STATE_SIZE];
-} t_game;
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	char		**map;
+	int			world_map[MAP_HEIGHT][MAP_WIDTH];
+	t_player	player;
+	t_img		img;
+	int			keys[KEY_STATE_SIZE];
+}	t_game;
 
 #endif
