7f7f8345ecd15c4a8b6f5a14acf59b6cec4a3734
misato <kawano.misato@gmail.com>
2025-12-27 09:15:00 +0900

    Revert "feat: implement parser module from main branch (#59)"
    This reverts commit 183fd3c0a3da9cfdd90ef576c327f2e730bbc5aa.

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -6,41 +6,17 @@
 /*   By: rnakatan <rnakatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 19:19:59 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/12/20 01:18:46 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/11/24 08:42:33 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parse.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "parse.h"
 
-/* Utils function prototypes */
-char	**read_map(const char *filename);
-void	free_map(void **map);
-
-/**
- * @brief メインのパース関数（検証のみ）
- * @param filepath .cubファイルのパス
- * @param game_data パース結果を格納する構造体（未使用、将来の拡張用）
- * @return 成功: 0 / 失敗: エラーコード
- */
-int	parse(const char *filepath, t_game_data *game_data)
+int	parse(const char **map)
 {
-	char	**input_data;
-	size_t	line_index;
-	int		ret;
-
-	(void)game_data;
-	line_index = 0;
-	input_data = read_map(filepath);
-	if (input_data == NULL)
-		return (-1);
-	if (validate_config(input_data, &line_index) != 0
-		|| validate_map(input_data, line_index) != 0)
-		ret = -1;
-	else
-		ret = load_data((const char **)input_data, game_data);
-	free_map((void **)input_data);
-	return (ret);
+	(void)map;
+	return (0);
 }
