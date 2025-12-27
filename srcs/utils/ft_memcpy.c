6c02b1d1a0ebf29b22a32310c73e4fe889613e4d
nytsdayo <nyantsutsu@gmail.com>
2025-12-27 15:17:24 +0900

    apply review

--- srcs/utils/ft_memcpy.c
+++ srcs/utils/ft_memcpy.c
@@ -6,27 +6,27 @@
 /*   By: rnakatan <rnakatan@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 00:00:00 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/12/27 00:00:00 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/12/27 15:17:04 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t			i;
-	unsigned char	*d;
-	const unsigned char	*s;
+void	*ft_memcpy(void *dst, const void *src, size_t n)  
+{  
+    size_t			i;  
+    unsigned char	*d;  
+    unsigned char	*s;  
 
-	if (dst == NULL && src == NULL)
-		return (NULL);
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
+    if (dst == NULL || src == NULL)  
+        return (NULL);  
+    d = (unsigned char *)dst;  
+    s = (unsigned char *)src;  
+    i = 0;  
+    while (i < n)  
+    {
+		d[i] = s[i];  
 		i++;
 	}
-	return (dst);
+	return (dst);  
 }
